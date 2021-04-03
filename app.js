587c6c2cac2645d81990d31ca259e0cb045c9817
AlonsoA-hub <uo270262@uniovi.es>
2021-04-03 15:27:24 +0200

    SDI-2021-603-8.2-Arquitectura para el acceso a datos.

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 app.use(express.static('public'));
 
 // Variables
@@ -14,9 +17,9 @@ app.set('port', 8081);
 app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.etxce.mongodb.net:27017,tiendamusica-shard-00-01.etxce.mongodb.net:27017,tiendamusica-shard-00-02.etxce.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-a4ued0-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
-require("./routes/rautores.js")(app, swig);
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rautores.js")(app, swig, gestorBD);
 
 app.listen(app.get('port'), function (){
     console.log("Servidor activo");
